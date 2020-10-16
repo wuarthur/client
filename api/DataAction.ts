4c950fc0cfbfd962c6208affe11bec770269dbb4
Manfred Xu <manfredxu99@gmail.com>
2020-10-15 20:30:10 -0700

    Cleanup

--- api/DataAction.ts
+++ api/DataAction.ts
@@ -3,46 +3,35 @@ import * as Data from './Data'
 import * as Utilities from './Utilities'
 
 export const addStudent = async (id: number, name: string, course: string, imgPath: string) => {
-    var targetBinaryData: string = await Utilities.convertImgToBinary(imgPath);
-    var body: Data.Student = {
-        courses: [course],
-        faceset_token: targetBinaryData,
-        name: name,
-        "student-id": id
-    }
-    axios({
-        method: 'POST',
-        url: 'http://127.0.0.1:1911/student',
-        data: body,
-        headers: { 'Content-Type': 'application/json' }
-    });
+  const targetBinaryData = await Utilities.convertImgToBinary(imgPath)
+  const body: Data.Student = {
+    courses: [course],
+    faceset_token: targetBinaryData,
+    name: name,
+    'student-id': id,
+  }
+  await axios({
+    method: 'POST',
+    url: 'http://127.0.0.1:1911/student',
+    data: body,
+    headers: { 'Content-Type': 'application/json' },
+  })
 }
 
-export const getStudentAttenceInfo = () => {
+export const getStudentAttenceInfo = () => {}
 
-}
+export const getStudentEnrollmentInfo = () => {}
 
-export const getStudentEnrollmentInfo = () => {
-
-}
-
-export const getStudents = (): Promise<Array<Data.Student>> => {
-    return new Promise((resolve, reject) => {
-        axios({
-            method: 'GET',
-            url: 'http://127.0.0.1:1911/student',
-            headers: { 'Content-Type': 'application/json' }
-        })
-            .then((response:AxiosResponse) => {
-                //handle success
-                console.log(response);
-                var students: Array<Data.Student> = response.data[0].results;
-                resolve(students);
-            })
-            .catch((error) => {
-                //handle error
-                console.log(error);
-                reject(error)
-            });
-    });
+export const getStudents = async (): Promise<Array<Data.Student>> => {
+  try {
+    const response = await axios({
+      method: 'GET',
+      url: 'http://127.0.0.1:1911/student',
+      headers: { 'Content-Type': 'application/json' },
+    })
+    return response.data[0].results
+  } catch (error) {
+    console.log(error)
+    throw error
+  }
 }
--- api/data.ts
+++ api/data.ts
@@ -1,18 +1,18 @@
 export interface Student {
-    courses: Array<string>,
-    faceset_token: string,
-    name: string,
-    'student-id': number
+  courses: Array<string>
+  faceset_token: string
+  name: string
+  'student-id': number
 }
 
-export enum className {
-    ENGLISH = "English",
-    CHINESE = "Chinese",
-    FARMING = "Farming",
-    ECONOMICS = "Economics"
+export enum ClassName {
+  ENGLISH = 'English',
+  CHINESE = 'Chinese',
+  FARMING = 'Farming',
+  ECONOMICS = 'Economics',
 }
 
 export enum Attendance {
-    ABSENT = 0,
-    ATTENDED
+  ABSENT = 0,
+  ATTENDED,
 }
