bd1f2a5d2c7d87dc17c8c41ac89d2bc75e2cbcdd
Paul Lam <paullam@Pauls-MacBook-Pro.local>
2020-10-15 21:21:26 -0700

    disabled passing api body  for GET

--- App/api/DataAction.ts
+++ App/api/DataAction.ts
@@ -1,5 +1,5 @@
 import axios, { AxiosRequestConfig, AxiosResponse, Method } from 'axios'
-import * as Data from './Data'
+import {Student, FetchedStudent, FetchedResult} from './Data'
 import * as Utilities from './Utilities'
 
 const BASE_URL = 'http://127.0.0.1:1911'
@@ -15,11 +15,12 @@ const performApiRequest = async <T>(
   config?: AxiosRequestConfig,
 ): Promise<AxiosResponse<T>> => {
   try {
+
     return await axiosInstance({
-      method,
+      method: method,
       url: route,
-      data,
-      ...config,
+      ...(method !== "GET" && {data: data}),
+      ...config
     })
   } catch (err) {
     console.log('API Error:', err)
@@ -29,11 +30,11 @@ const performApiRequest = async <T>(
 
 export const addStudent = async (id: number, name: string, course: string, imgPath: string) => {
   const targetBinaryData = await Utilities.convertImgToBinary(imgPath)
-  const body: Data.Student = {
+  const body: FetchedStudent = {
     courses: [course],
     faceset_token: targetBinaryData,
     name: name,
-    'student-id': id,
+    'student-id': id.toString(),
   }
   return performApiRequest('POST', '/student', body, {
     headers: { 'Content-Type': 'application/json' },
@@ -44,16 +45,33 @@ export const getStudentAttenceInfo = () => {}
 
 export const getStudentEnrollmentInfo = () => {}
 
-export const getStudents = async (): Promise<Array<Data.Student>> => {
+export const getStudents = async (): Promise<Array<Student>> => {
   try {
-    const response = await performApiRequest(
+    const response: AxiosResponse<Array<FetchedResult>> = await performApiRequest(
       'GET',
       '/student',
       {},
       { headers: { 'Content-Type': 'application/json' } },
     )
-    console.log('Students response:', response)
-    return response.data[0].results
+
+    console.log("student response");
+    console.log(response.data[0].results);
+    var students: Array<Student> = [];
+
+    if (!!response.data && response.data.length > 0 && !!response.data[0].results) {
+        students = response.data[0].results.map((fetchedStudent: FetchedStudent) => {
+            return {
+                id: parseInt(fetchedStudent["student-id"]),
+                name: fetchedStudent.name,
+                faceset_token: fetchedStudent.faceset_token
+            }
+        });
+        console.log(students);
+    }
+
+    return students;
+
+    
   } catch (error) {
     console.log(error)
     throw error
--- App/api/data.ts
+++ App/api/data.ts
@@ -1,9 +1,31 @@
-export interface Student {
+export interface FetchedResult {
+  results?: Array<FetchedStudent>
+}
+
+export interface FetchedStudent {
   courses: Array<string>
   faceset_token: string
   name: string
-  'student-id': number
+  'student-id': string
 }
+
+export interface Student {
+    id: number,
+    name: string,
+    faceset_token: string,
+    attendance?: number
+}
+
+export interface Class {
+    name: string,
+    teacher: string,
+    students: Array<Student>
+}
+
+export interface StudentAttendanceListResult {
+    classes: Array<Class>
+}
+
 
 export enum ClassName {
   ENGLISH = 'English',
